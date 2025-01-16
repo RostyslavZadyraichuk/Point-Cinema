de5f6f54324c9adba7e23b13308d4c580216ca28
Rostyslav Zadyraichuk <rostykzadyraichuk010@gmail.com>
2025-01-16 18:00:03 +0200

    Add entities-enumertaions

---
+++ src/main/java/com/zadyraichuk/point_cinema/entity/Day.java
@@ -0,0 +1,13 @@
+package com.zadyraichuk.point_cinema.entity;
+
+public enum Day {
+
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+
+}
---
+++ src/main/java/com/zadyraichuk/point_cinema/entity/Genre.java
@@ -0,0 +1,20 @@
+package com.zadyraichuk.point_cinema.entity;
+
+public enum Genre {
+
+    ACTION,
+    COMEDY,
+    CARTOON,
+    ROMANCE,
+    CRIMINAL,
+    SCIENCE_FICTION,
+    DOCUMENTARY,
+    HORROR,
+    FANTASY,
+    ADVENTURE,
+    DETECTIVE,
+    THRILLER,
+    HISTORICAL,
+    DRAMA
+
+}
